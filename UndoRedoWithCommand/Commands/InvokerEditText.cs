2327dbe23e027d1b546e8644d08f904acef0904b
pouria_meftahi <pouriameftahi@hotmail.com>
2023-05-22 22:09:05 +0330

    Add project files.

---
+++ UndoRedoWithCommand/Commands/InvokerEditText.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndoRedoWithCommand.Commands
+{
+    public class InvokerEditText
+    {
+
+        Reciver_EditText editText = new Reciver_EditText();
+        List<Command> commands = new List<Command>();
+        int current = 0;
+
+        public string ExecuteCommand(string input)
+        {
+            Command command = new Concreate_AddTextToLable(editText);
+            string result = command.Execute(input);
+
+            commands.Add(command);
+            current++;
+
+            return result;
+        }
+
+
+        public string Undo()
+        {
+            Command command = commands[--current];
+            return command.UnExecute(command.CurrentInput);
+        }
+
+        public string Redo()
+        {
+            Command command = commands[current++];
+            return command.Execute(command.CurrentInput);
+        }
+    }
+}
---
+++ UndoRedoWithCommand/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UndoRedoWithCommand.Commands;
+
+namespace UndoRedoWithCommand
+{
+    public partial class Form1 : Form
+    {
+
+        InvokerEditText invokerEditText;
+        public Form1()
+        {
+
+            invokerEditText = new InvokerEditText();
+            InitializeComponent();
+        }
+
+        private void btnSetText_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("لطفا مقدار را وارد کنید");
+            }
+            else
+            {
+                label1.Text = invokerEditText.ExecuteCommand(textBox1.Text);
+                textBox1.Text = "";
+            }
+        }
+
+        private void Undo_Click(object sender, EventArgs e)
+        {
+            label1.Text = invokerEditText.Undo();
+        }
+
+        private void Redo_Click(object sender, EventArgs e)
+        {
+            label1.Text = invokerEditText.Redo();
+        }
+    }
+}
